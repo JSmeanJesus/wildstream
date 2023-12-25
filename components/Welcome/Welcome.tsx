8aa56f30c376d555d4bc424ba828103f817a40fa
Nick <tenebrissfer@gmail.com>
2023-12-25 11:47:48 +0200

    Initial commit

---
+++ components/Welcome/Welcome.tsx
@@ -0,0 +1,18 @@
+import { Title, Text } from '@mantine/core';
+import classes from './Welcome.module.css';
+
+export function Welcome() {
+  return (
+    <>
+      <Title className={classes.title} ta="center" mt={100}>
+        Welcome to{' '}
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'violet' }}>
+          Wildstream
+        </Text>
+      </Title>
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+        Мы делаем стримы комфортными.
+      </Text>
+    </>
+  );
+}
